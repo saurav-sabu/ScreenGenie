cd71aaaf6357aba6be675bd0c17c73800110a847
Saurav Sabu <saurav.sabu9@gmail.com>
2024-12-02 00:50:28 +0530

    added code in setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="ScreenGenie",
+    version="0.0.1",
+    author="Saurav Sabu",
+    author_email="saurav.sabu9@gmail.com",
+    packages=find_packages()
+)
+
